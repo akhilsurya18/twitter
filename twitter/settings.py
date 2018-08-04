a518e2ef20f467638b394a1cfc28cd880b771a6e
akhilsurya18 <akhil.7stars@gmail.com>
2018-08-04 20:04:26 +0530

    commit

--- twitter/settings.py
+++ twitter/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,7 +80,7 @@ WSGI_APPLICATION = 'twitter.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-A
+
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -91,7 +90,8 @@ DATABASES = {
         'PASSWORD': "mysqlpassword1",
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
